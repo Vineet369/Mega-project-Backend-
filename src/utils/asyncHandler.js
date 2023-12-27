87a97444c268ba208f32ab1d69fa563834b165b9
vineet <vineetmaurya.4985@gmail.com>
2023-12-27 20:16:49 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 // BY PROMISES
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
+    return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
     }
 }
